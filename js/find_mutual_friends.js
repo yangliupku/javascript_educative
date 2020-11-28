e22e362189dbd6ebd8ce20ea490fa5a5091a8f07
Yang Liu <yliupku@yliupku-mbp.dhcp.thefacebook.com>
2020-11-27 23:57:01 -0500

    Add mapper

--- js/find_mutual_friends.js
+++ js/find_mutual_friends.js
@@ -32,4 +32,26 @@ function setup(arr) {
   return friends;
 }
 
-module.exports = { setup };
+/**
+ *  map each property of the object representation of the friends
+ * inputs = { 'A' : ['B', 'C', 'D'] }
+ * outputs = [
+  { 'AB' : ['B', 'C', 'D'] },
+  { 'AC' : ['B', 'C', 'D'] },
+  { 'AD' : ['B', 'C', 'D'] },
+ ]
+ */
+function mapper(setup_arr) {
+  var friends_arr = [];
+  for (key in setup_arr) {
+    var friends = setup_arr[key];
+    friends_arr = friends_arr.concat(
+      friends.map(function (name) {
+        var joint_key = [key, name].sort().reduce((prev, curr) => prev + curr);
+        return { [joint_key]: [...friends] };
+      })
+    );
+  }
+  return friends_arr;
+}
+module.exports = { setup, mapper };
--- tests/find_mutual_friends.test.js
+++ tests/find_mutual_friends.test.js
@@ -15,3 +15,31 @@ test("test setup", () => {
     D: ["A", "B"],
   });
 });
+
+test("test mapping single", () => {
+  expect(mutual_friendts.mapper({ A: ["B", "C", "D"] })).toStrictEqual([
+    { AB: ["B", "C", "D"] },
+    { AC: ["B", "C", "D"] },
+    { AD: ["B", "C", "D"] },
+  ]);
+});
+
+test("test mapping multiple", () => {
+  expect(
+    mutual_friendts.mapper({
+      A: ["B", "C", "D"],
+      B: ["A", "D"],
+      C: ["A"],
+      D: ["A", "B"],
+    })
+  ).toStrictEqual([
+    { AB: ["B", "C", "D"] }, // from 'A'
+    { AC: ["B", "C", "D"] }, // from 'A'
+    { AD: ["B", "C", "D"] }, // from 'A'
+    { AB: ["A", "D"] }, // from 'B'
+    { BD: ["A", "D"] }, // from 'B'
+    { AC: ["A"] }, // from 'C'
+    { AD: ["A", "B"] }, // from 'D'
+    { BD: ["A", "B"] }, // from 'D'
+  ]);
+});
